d8dbef0d5b12d1debc3779ce6a6027f840164ef6
Paresh Chouhan <paresh@cumul.io>
2022-07-11 17:00:17 +0200

    Added base code to integrate cumulio-dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-hello-world';
+  dashboardId='DASHBOARD_ID_HERE';
+  key?: string;
+  token?: string;
+
+  constructor(
+    private http: HttpClient,
+  ) {}
+
+  ngInit() {
+    this.http.get<{
+      status: string;
+      key?: string;
+      token?: string;
+    }>('http://localhost:4001').subscribe((data) => {
+      console.log(data);
+      this.key = data.key;
+      this.token = data.token;
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgxCumulioDashboardModule } from '@cumul.io/ngx-cumulio-dashboard';
 import { AppComponent } from './app.component';
-
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxCumulioDashboardModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
